d2819138a55b507fdd78bd8ae35bc19b45eba9bd
Rodrigo Carneiro Andrade <andrade.rodrigo@USCMAC192.local>
2022-01-09 12:48:30 -0300

    refactor: user-dto-date-refactor

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,9 +1,11 @@
+import { Type } from 'class-transformer';
 import { IsBoolean, IsDate, IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   readonly full_name: string;
 
+  @Type(() => Date)
   @IsDate()
   readonly date_of_birth: Date;
 
@@ -16,6 +18,7 @@ export class CreateUserDto {
   @IsString()
   readonly password: string;
 
+  @Type(() => Date)
   @IsDate()
   readonly last_access: Date;
 }
