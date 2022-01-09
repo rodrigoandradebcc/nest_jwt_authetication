144ce11d52d2a62f70e475bdb71bcc10206afd9d
Rodrigo Carneiro Andrade <andrade.rodrigo@USCMAC192.local>
2022-01-09 04:28:33 -0300

    feat: auth create files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 import * as ormOptions from './config/orm';
 
 @Module({
-  imports: [CoursesModule, TypeOrmModule.forRoot(ormOptions), UsersModule],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot(ormOptions),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
