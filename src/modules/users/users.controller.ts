b755a9baee62848d802e64fbe2a12c25486fdfc0
Rodrigo Carneiro Andrade <andrade.rodrigo@USCMAC192.local>
2022-01-09 04:15:52 -0300

    chore: i]nstall dependencies

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body: CreateUserDto) {
+    return this.usersService.create(body);
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,22 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { create } from 'domain';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
   ) {}
-  // create(createCourseDTO: CreateUserDto) {
-  //     const user = this.usersRepository.create(createCourseDTO);
-  //     return this.usersRepository.save(user);
-  //   }
-  create(createCourseDTO: CreateUserDto) {
-    const user = this.usersRepository.create(createCourseDTO);
+
+  findOne(id: string) {
+    const course = this.usersRepository.findOne(id);
+
+    if (!course) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+
+    return course;
+  }
+
+  findByEmail(email: string) {
+    const course = this.usersRepository.findOne(email);
+
+    if (!course) {
+      throw new NotFoundException(`User ${email} not found`);
+    }
+
+    return course;
+  }
+
+  async create(createCourseDTO: CreateUserDto) {
+    const { password } = createCourseDTO;
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+      ...createCourseDTO,
+      password: encryptedPassword,
+    };
+
+    const user = this.usersRepository.create(newUser);
     return this.usersRepository.save(user);
   }
 }
