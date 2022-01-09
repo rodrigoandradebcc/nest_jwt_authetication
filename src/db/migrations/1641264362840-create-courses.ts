24fcac960fd1af72ae7bccb5688b5d2df33ae859
Rodrigo Carneiro Andrade <andrade.rodrigo@USCMAC192.local>
2022-01-09 14:49:20 -0300

    refactor: migrations

--- src/db/migrations/1641264362840-create-courses.ts
+++ src/db/migrations/1641264362840-create-courses.ts
@@ -6,10 +6,10 @@ export class createCourses1641264362840 implements MigrationInterface {
     columns: [
       {
         name: 'id',
-        type: 'integer',
+        type: 'uuid',
         isPrimary: true,
-        isGenerated: true,
-        generationStrategy: 'increment',
+        generationStrategy: 'uuid',
+        default: 'uuid_generate_v4()',
       },
       {
         name: 'name',
@@ -27,6 +27,8 @@ export class createCourses1641264362840 implements MigrationInterface {
   });
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
     await queryRunner.createTable(this.table);
   }
 
